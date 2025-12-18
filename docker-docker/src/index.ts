a463f3adc3aaada124a05e9242f1eb32242f72f4
Koko-atp <koko.atp25@gmail.com>
2025-12-18 10:33:35 +0700

    trytry

---
+++ docker-docker/src/index.ts
@@ -0,0 +1,20 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/dd/:d' , (c) => {
+  return c.html('<h1> yo <h1>')
+})
+
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
